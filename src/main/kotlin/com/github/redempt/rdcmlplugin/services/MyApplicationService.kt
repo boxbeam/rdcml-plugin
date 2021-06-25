cf13e92f084c4b6c96004d7595067506e183fa82
GitHub Action <action@github.com>
2021-06-25 00:19:07 +0000

    Template cleanup

--- src/main/kotlin/com/github/redempt/rdcmlplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/redempt/rdcmlplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.redempt.rdcmlplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.redempt.rdcmlplugin.MyBundle
 
 class MyApplicationService {
 
